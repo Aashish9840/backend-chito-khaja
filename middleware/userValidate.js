814a0b1cdbe3a042c9e2b7842700add7cc8a6d05
Aashish Shah <shahaashih@gmail.com>
2025-05-18 21:52:58 +0545

    changes on middleware

--- middleware/userValidate.js
+++ middleware/userValidate.js
@@ -23,6 +23,7 @@ const userValidate = async (req, res, next) => {
   }
 };
 
+
 const isAdmin = (req, res, next) => {
   if (req.user?.role !== "admin") {
     return res.status(403).json({ message: "Access denied: Admins only" });

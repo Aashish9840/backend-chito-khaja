3aedffa3f42f93954233c67204e7da921f9f2757
aashish <shahaashih@gmail.com>
2025-07-08 21:06:25 +0545

    connected atlas database

--- configuration/mongoDBConnection.js
+++ configuration/mongoDBConnection.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
+console.log(process.env.MONGODB_STRING)
 const connectMongo = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_STRING);
